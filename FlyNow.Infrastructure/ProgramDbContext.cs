6206bc5cae16d610e79dc5525798b0d187229664
Danilo Riguette <contato.danilo8201295@gmail.com>
2024-10-30 23:29:16 -0300

    Configuração inicial da API e da Infrastructure

---
+++ FlyNow.Infrastructure/ProgramDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FlyNow.Infrastructure
+{
+	public class ProgramDbContext : DbContext
+	{
+		DbContextOptions<ProgramDbContext> options;
+
+		public ProgramDbContext(DbContextOptions<ProgramDbContext> options) : base(options) { }
+
+	}
+}
--- FlyNow/Program.cs
+++ FlyNow/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using FlyNow.Infrastructure;
+using System.Configuration;
 
-namespace FlyNow
+
+var builder = WebApplication.CreateBuilder(args);
+
+ConfigureServices(builder);
+
+void ConfigureServices(WebApplicationBuilder builder)
 {
-	class Program
-	{
-		public static void Main(string[] args) { 
-			Console.WriteLine("Hello");
-		}
+	builder.Services.AddControllers();
 
-	}
+	builder.Services.AddEndpointsApiExplorer();
+	builder.Services.AddSwaggerGen();
+
+	var con = builder.Configuration.GetConnectionString("ConexaoMySql");
+	builder.Services.AddDbContext<ProgramDbContext>( options => {
+		options.UseMySql(con, ServerVersion.AutoDetect(con)); 
+		}
+	);
 }
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
